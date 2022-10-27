94969a3e38fba37f0d9be194a499c6c1d46c3f3c
dervishllari <kelvin.dervishllari@moveoalbania.com>
2022-10-27 11:45:33 +0200

    structure

---
+++ src/test/java/com/example/hazelacastexample/HazelacastExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hazelacastexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelacastExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
